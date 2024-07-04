438b069724560368f3902242b8254c25eca08dc5
Ben-Learner <qiben14zdq@gmail.com>
2024-07-04 18:05:51 +0800

    update

--- llm.py
+++ llm.py
@@ -1,6 +1,27 @@
 import streamlit as st
 
 # Create the LLM
+from langchain_openai import ChatOpenAI
+
+llm = ChatOpenAI(
+    model=st.secrets["OPENAI_MODEL"],
+    api_key=st.secrets["OPENAI_API_KEY"],
+    base_url = st.secrets["BASE_URL"]
+)
+
+# messages = [
+#     ("system", "You are a helpful translator. Translate the user sentence to French."),
+#     ("human", "I love programming."),
+# ]
+
+# response = llm.invoke(messages)
+# print(response)
 
 
 # Create the Embedding model
+from langchain_openai import OpenAIEmbeddings
+
+embeddings = OpenAIEmbeddings(
+    openai_api_key = st.secrets["OPENAI_API_KEY"],
+    base_url = st.secrets["BASE_URL"]
+)
